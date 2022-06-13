8692f9bc00e42ffab80d9148d08318f3aaed2919
Patra <patra@eigen.co.id>
2022-06-13 21:08:16 +0700

    init-project(laravel): init with my base laravel, you can see from my Github repo list

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\BlogController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthAPI;
@@ -17,18 +16,6 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::post('logout', 'logout');
         Route::get('user', function (Request $request) {
             return $request->user();
-        });
-    });
-
-    Route::controller(BlogController::class)->group(function () {
-        Route::prefix('blogs')->group(function () {
-            Route::put('active', 'active');
-            Route::put('inactive', 'inactive');
-            Route::get('', 'index');
-            Route::get('{id}', 'show');
-            Route::post('', 'create');
-            Route::put('{id}', 'update');
-            Route::delete('', 'delete');
         });
     });
 });
